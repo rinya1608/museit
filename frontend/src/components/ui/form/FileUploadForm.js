88d5556fca813b26eb606e5dc7fe7bafaebf2972
renat <rinya1608@gmail.com>
2022-03-27 14:23:27 +0500

    MUS-26 add feedback button

--- frontend/src/components/ui/form/FileUploadForm.js
+++ frontend/src/components/ui/form/FileUploadForm.js
@@ -39,14 +39,16 @@ const FileUploadForm = React.forwardRef((props, ref) => {
             </div>
             <form className={"fileUploadForm-form"}>
                 {!props.blob
-                    ? <div>
+                    ? <div className={"fileUploadForm-form-wrap"}>
                         <button className={"fileUploadForm-form_button-file"} id={"fileUploadFormInput"} onClick={chooseFile}>Выбрать файл</button>
                         <input className={"fileUploadForm-form_input-file"} type="file" multiple={false} ref={ref} onChange={fileUploadHandler}/>
                         {
                             isFile ? <button onClick={props.sendClick} className={"fileUploadForm-form_button-download"}>Загрузить</button> : ''
                         }
                 </div>
-                    : <button onClick={event => downloadFile(event,props.blob,fileName)}>Cкачать</button>}
+                    : <button onClick={event => downloadFile(event,props.blob,fileName)}>Cкачать</button>
+                }
+                <div className={"fileUploadForm-form_feedback-wrap"}><a className={"fileUploadForm-form_feedback"} href={"https://t.me/+fsdwBE3o6_o0YmJi"}>Give feedback in Telegram</a></div>
             </form>
         </div>
     );
