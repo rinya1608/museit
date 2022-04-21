dfda6797301c6a278eff3d3bba5cafcb2c0b2ab1
Anton <amaslov455@gmail.com>
2022-04-21 22:55:40 +0500

    added sending simple email message without file

--- src/main/java/ru/web/museit/controller/FeedbackController.java
+++ src/main/java/ru/web/museit/controller/FeedbackController.java
@@ -1,16 +1,32 @@
 package ru.web.museit.controller;
 
+import javax.mail.MessagingException;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.web.museit.request.FeedbackRequest;
+import ru.web.museit.service.SendEmail;
 
 @RestController
 public class FeedbackController {
+
+    @Autowired
+    private SendEmail sendEmail;
+
     @PostMapping(path = "api/feedback")
-    public ResponseEntity<?> sendFeedback(@ModelAttribute FeedbackRequest feedbackRequest) {
+    public ResponseEntity<?> sendFeedback(@ModelAttribute FeedbackRequest feedbackRequest) throws MessagingException {
         System.out.println(feedbackRequest);
+        sendEmail.sendMailWithFile(
+            feedbackRequest.getName(),
+            feedbackRequest.getContact(),
+            feedbackRequest.getMessage(), 
+            feedbackRequest.getProcessedFile(),
+            "Отзыв от пользователя"
+        );
+        System.out.println(feedbackRequest.getProcessedFile());
         return ResponseEntity.ok().build();
     }
 }
---
+++ src/main/java/ru/web/museit/service/SendEmail.java
@@ -0,0 +1,44 @@
+package ru.web.museit.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+@Service
+public class SendEmail {
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+    
+    public void sendMailWithFile(String name,
+                                 String contact,
+                                 String body,
+                                 MultipartFile file,
+                                 String subject) throws MessagingException{
+        
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+        MimeMessageHelper mimeMessageHelper =  new MimeMessageHelper(mimeMessage, MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED);
+
+        String full_text = new StringBuilder()
+        .append("Имя пользователя: " + name + "\n")
+        .append("email/телефон: " + contact + "\n")
+        .append("\n")
+        .append(body)
+        .toString();
+
+        mimeMessageHelper.setFrom("museitcorp@gmail.com");
+        mimeMessageHelper.setTo("amaslov455@gmail.com");
+        mimeMessageHelper.setText(full_text, false);
+        mimeMessageHelper.setSubject(subject);
+
+        javaMailSender.send(mimeMessage);
+
+        System.out.println("Mail sended!");
+                                
+    }
+}
